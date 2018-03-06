9fb8c243b0246de185cc569abf1e982955825d7a
Vladdd97 <vladbantus@mail.ru>
2018-03-07 01:39:57 +0200

    Laboratory work 2

---
+++ Laboratory work 2/WebTechnology/WebTechnology.movieSite/Controllers/CartoonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTechnology.movieSite.Controllers
+{
+    public class CartoonController : Controller
+    {
+        // GET: Cartoon
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Laboratory work 2/WebTechnology/WebTechnology.movieSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebTechnology.movieSite.MvcApplication" Language="C#" %>
